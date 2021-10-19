0e4e53ef56532d6454d71629631abce571d8d801
Baidal <lvr31@alu.ua.es>
2021-10-19 16:18:13 +0200

    Añadidos votos a posts

--- src/controllers/Post.js
+++ src/controllers/Post.js
@@ -2,6 +2,7 @@ const Post = require("../models").Post;
 const Comment = require("../models").Comment;
 const User = require("../models").User;
 const Community = require("../models").Community;
+const Vote = require("../models").Vote;
 
 module.exports = {
   async getPost(req, res) {
@@ -71,27 +72,111 @@ module.exports = {
           errores: [{ error: `No se ha introducido el título del post` }],
         });
         return;
-
       }
-      
+
       const new_post = await Post.create({
         title,
         text,
         UserId: res.locals.user.id,
-        CommunityId: community.dataValues.id
+        CommunityId: community.dataValues.id,
       });
 
-      new_post.dataValues.numComments = 0
-      new_post.dataValues.comments = []
-      new_post.dataValues.userNick = res.locals.user.nick
-      new_post.dataValues.communityName = community.dataValues.name
+      new_post.dataValues.numComments = 0;
+      new_post.dataValues.comments = [];
+      new_post.dataValues.userNick = res.locals.user.nick;
+      new_post.dataValues.communityName = community.dataValues.name;
 
       res.status(201).send({
         new_post,
       });
     } catch (e) {
       console.log(
-        "Se ha producido un error en 'createPost' del controlador 'Post': \n" + e
+        "Se ha producido un error en 'createPost' del controlador 'Post': \n" +
+          e
+      );
+
+      res.status(500).send({
+        errors: [{ error: "error interno en el servidor" }],
+      });
+    }
+  },
+  async votePost(req, res) {
+    try {
+      const postId = req.params.id;
+
+      const post = await Post.findOne({ where: { id: postId } });
+
+      if (!post) {
+        res.status(404).send({
+          errores: [{ error: `No se ha encontrado el post con id ${postId}` }],
+        });
+        return;
+      }
+
+      const value = req.body.value;
+
+      if (!value) {
+        res.status(400).send({
+          errores: [{ error: "No se ha introducido el valor del post" }],
+        });
+        return;
+      }
+
+      if(isNaN(value) || (value !== -1 && value !== 1)) {
+        res.status(400).send({
+          errores: [{ error: "El valor del post no es correcto" }],
+        });
+        return;
+      }
+
+      const current_vote = await Vote.findOne({
+        where: {
+          UserId: res.locals.user.id,
+          PostId: post.dataValues.id
+        }
+      })
+
+      //El usuario ya había votado en el post
+      if(current_vote){
+        //El usuario ha votado lo mismo que ya tenía
+        if(current_vote.value === value){
+          res.status(200).send({post})
+          return
+        }else{
+          //El usuario cambia de voto
+          current_vote.value = value
+          current_vote.save()
+
+          /**
+           * Actualizamos el valor de 'votes' de post. Tener en cuenta que al cambiar de voto,
+           * estamos quitando el voto que ya tenía y le estamos añadiendo otro. Esto es, si el
+           * voto que ya tenía era negativo, y este tenia un 'votes' de 14, le quitamos el voto negativo,
+           * 'votes' pasa a ser 15 y luego le añadimos el voto positivo, por lo que pasa a valer 16
+           */
+          post.votes += value*2;
+          post.save()
+        }
+      }else {
+        //El usuario aun no ha votado en el post
+        Vote.create({
+          UserId: res.locals.user.id,
+          PostId: post.id,
+          value: value
+        })
+        
+        //Actualizamos el valor de 'votes' de post
+        post.votes += value
+        post.save()
+      }
+      
+      res.status(200).send({
+        post
+      })
+
+    } catch (e) {
+      console.log(
+        "Se ha producido un error en 'votePost' del controlador 'Post': \n" +
+          e
       );
 
       res.status(500).send({
--- src/routes/Post.js
+++ src/routes/Post.js
@@ -4,4 +4,5 @@ const authChecker = require('../utils/auth')
 module.exports = (app) => {
     app.get('/api/post/:id', postController.getPost)
     app.post('/api/comunidad/:com_name/post',authChecker,postController.createPost)
+    app.post('/api/post/:id/vote', authChecker ,postController.votePost)
 }
