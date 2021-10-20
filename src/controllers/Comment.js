29faca6e53f872a66b194818591d067a1283184d
Baidal <lvr31@alu.ua.es>
2021-10-20 12:42:00 +0200

    Acabada creación de comentarios

--- src/controllers/Comment.js
+++ src/controllers/Comment.js
@@ -1,45 +1,74 @@
-const Comment = require('../models').Comment
-const Post = require('../models').Post
+const Comment = require("../models").Comment;
+const Post = require("../models").Post;
 
 module.exports = {
-    async createComment(req,res){
-        const id = req.params.id
+  async createComment(req, res) {
+    try {
+      const id = req.params.id;
+      const userId = res.locals.user.id
+      const post = await Post.findByPk(id);
 
-        const post = await Post.findByPk(id)
+      if (!post) {
+        res.status(404).send({
+          errores: [{ error: `No se ha encontrado el post con id ${id}` }],
+        });
 
-        if(!post) {
-            res.status(404).send({
-                errores: [
-                    {error: `No se ha encontrado el post con id ${id}`}
-                ]
-            })
+        return;
+      }
 
-            return
+      const { text, commentId } = req.body;
+
+      if (!text) {
+        res.status(400).send({
+          errores: [{ error: `No se ha introducido el texto del comentario` }],
+        });
+
+        return;
+      }
+
+      //Creamos el comentario como subcomentario
+      if (commentId && commentId !== -1) {
+        const actualComment = await Comment.findByPk(commentId);
+
+        if (!actualComment) {
+          res.status(404).send({
+            errores: [
+              {
+                error: `No se ha encontrado el comentario con id ${commentId}`,
+              },
+            ],
+          });
+          return;
         }
 
-        const {text, commentId} = req.body
+        const new_subcomment = await post.createComment({
+            text,
+            UserId: userId
+        });
 
-        if(!text) {
-            res.status(400).send({
-                errores: [
-                    {error: `No se ha introducido el texto del comentario`}
-                ]
-            })
+        actualComment.addSubComment(new_subcomment)
 
-            return
-        }
+        res.status(201).send({
+          new_subcomment,
+        });
+      } else {
+        //creamos el comentario como nuevo
+        const new_comment = await post.createComment({
+          UserId: userId,
+          text,
+        });
 
-        //Creamos el comentario como subcomentario
-        if(commentId && commentId !== -1) {
+        res.status(201).send({ new_comment });
+      }
+    } catch (e) {
+      console.log(
+        "Se ha producido un error en 'createComment' del controlador 'Comment': \n" +
+          e
+      );
 
-        } else {
-            //creamos el comentario como nuevo
-            const new_comment = await post.createComment({
-                UserId: res.locals.user.id,
-                text
-            })
-
-            res.status(201).send({new_comment})
-        }
+      res.status(500).send({
+        errors: [{ error: "error interno en el servidor" }],
+      });
     }
-} 
+  },
+};
