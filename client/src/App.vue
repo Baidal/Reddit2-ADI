2de92c319259ffdc14f03fe1617bb6e53336eed8
Baidal <lvr31@alu.ua.es>
2021-11-19 01:38:52 +0100

    Modificado cliente para que no utilice vite

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,18 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div class="max-w-md mx-auto inline-flex justify-center">
-    <img alt="Vue logo" src="./assets/logo.png" />
-  </div>
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
 
 <style>
 #app {
