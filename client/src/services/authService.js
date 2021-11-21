01739d2c947700339734af3f26b2257873991ad7
Baidal <lvr31@alu.ua.es>
2021-11-21 23:35:45 +0100

    Avanzado en el login y registro

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -3,13 +3,19 @@ import urls from './apiUrl'
 
 const login = urls.DEFAULT_API + 'login'
 
-module.exports = {
+export default {
     login(nickEmail, password){
-        axios.post(login, {
+       return axios.post(login, {
             nickEmail,
             password
         }).then(resp => { //el login ha sido correcto
             localStorage.setItem('user', JSON.stringify(resp.data))
         }).finally(resp => resp.data)
+    },
+    logout(){
+        localStorage.removeItem('user')
+    },
+    register(userData) {
+        
     }
 }
--- client/src/store/auth.js
+++ client/src/store/auth.js
@@ -1,13 +1,45 @@
-const actualUser = JSON.parse(localStorage.getItem('user'));
-const initialState = actualUser ? {loggedIn: true, actualUser } : {loggedIn: false, actualUser: null}
+import authService from '../services/authService'
+
+const actualUser = JSON.parse(localStorage.getItem("user"));
+const initialState = actualUser
+    ? { loggedIn: true, actualUser }
+    : { loggedIn: false, actualUser: null };
 
 export default {
     namespaced: true,
     initialState: initialState,
     actions: {
-
+        async login({ commit }, {nickEmail, password}) {
+            const userData = await authService.login(nickEmail, password)
+            
+            if(userData.errores)
+                commit('loginFailure')
+            else
+                commit('loginSuccess', userData)
+        },
+        logout({ commit }) {
+            commit('logout')
+            authService.logout()
+        }
     },
     mutations: {
-
-    }
-}
+        loginSuccess(state, user) {
+            state.loggedIn = true;
+            state.actualUser = user;
+        },
+        loginFailure(state) {
+            state.loggedIn = false;
+            state.actualUser = null;
+        },
+        logout(state) {
+            state.loggedIn = false;
+            state.actualUser = null;
+        },
+        registerSuccess(state) {
+            state.loggedIn = false;
+        },
+        registerFailure(state) {
+            state.loggedIn = false;
+        },
+    },
+};
