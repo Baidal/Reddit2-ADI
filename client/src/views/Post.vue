837395ae726e7da65badbbdc477e8ffe04c4a296
Baidal <lvr31@alu.ua.es>
2021-12-06 19:01:27 +0100

    Añadido scroll infinito en los comentarios de un post

--- client/src/views/Post.vue
+++ client/src/views/Post.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="block w-1/2 mx-auto space-y-6">
+    <div class="block w-1/2 mx-auto space-y-6 mb-10">
         <PostCard :post="post"/>
-        <div class="space-y-3 bg-dark-grey p-4 rounded border border-light-grey m-3 mb-40">
+        <div class="space-y-3 bg-dark-grey p-4 rounded border border-light-grey m-3">
             <form class="space-y-3" @submit.prevent="newComment">
                 <textarea v-model="new_comment" class="box-border pl-4 pt-2 block w-full bg-medium-gray h-24 border border-light-grey focus:outline-none focus:ring-1 focus:ring-gray-600 text-gray-300" placeholder="Nuevo comentario..."></textarea>
                 <button class="bg-white-gray rounded-full px-3 py-1 text-xs font-bold" :class="{'cursor-not-allowed': this.canComment(), 'text-gray-400': this.canComment()}">Comentar</button>
@@ -27,7 +27,8 @@ export default {
             postId: this.$route.params.id,
             commentPage: 1,
             commentLimit: 15,
-            new_comment: ''
+            new_comment: '',
+            noMoreComments: false
         };
     },
     components: {
@@ -36,6 +37,9 @@ export default {
     },
     async beforeMount() {
         this.getPostData().then(res => this.post = res.data.post).catch(() => this.$router.push({name: 'error', params: {error: `No se ha encontrado el post con id ${this.postId}`}}))
+    },
+    mounted(){
+        this.getNextComments()
     },
     methods: {
         getPostData(){
@@ -59,6 +63,23 @@ export default {
         async newSubComment(props){
             const response = await commentService.createComment(props.postId, props.new_comment, props.commentId)
             this.post.Comments[this.post.Comments.findIndex((comment) => comment.id == props.commentId)].subComments.unshift(response.new_subcomment)
+        },
+        getNextComments(){
+            window.onscroll = async () => {
+                //Sacado de https://www.digitalocean.com/community/tutorials/vuejs-implementing-infinite-scroll 
+                const bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
+                if (bottomOfWindow && !this.noMoreComments) {
+                    
+                    this.commentPage++;
+
+                    const new_comments = (await this.getPostData()).data.post.Comments
+                    this.post.Comments = this.post.Comments.concat(new_comments)
+
+                    //no hay mas posts
+                    if(new_comments.length === 0)
+                        this.noMoreComments = true
+                }
+            }
         }
     },
 };
