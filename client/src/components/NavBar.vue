c08820d2626476e365e882dbb40deadc0b4e58e5
Baidal <lvr31@alu.ua.es>
2021-12-22 22:21:38 +0100

    Modificado navbar

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,18 +1,26 @@
 <template>
     <nav class="flex justify-between bg-dark-grey p-6 text-gray-300 w-full top-0 fixed z-50">
         <router-link to="/">
-            <div class="flex items-center flex-shrink-0 mr-6">
+            <div class="flex items-center flex-shrink-0 mr-6 border-red-400">
                 
                     <img alt="Reddit Logo" src="../assets/reddit.png" class="h-14">
                     <p>Reddit 2</p>
                 
             </div>
         </router-link>
-        <div class="flex rounded-lg w-1/4 my-auto bg-light-grey h-10 hover:border-gray-400 focus:border-gray-400 border border-gray-300">
-            <div class="my-auto pl-4">
-                <SearchIcon class="w-5 h-5"/>
+        <div class="w-1/4 my-auto flex space-x-2">
+            <div class="flex rounded-sm w-3/4  bg-light-grey h-10 hover:border-gray-400 focus:border-gray-400 border border-gray-300">
+                <div class="my-auto pl-4">
+                    <SearchIcon class="w-5 h-5"/>
+                </div>
+                <input class="block pl-4 bg-light-grey  rounded-md focus:outline-none w-full" placeholder="Buscar en reddit...">
             </div>
-            <input class="block pl-4 bg-light-grey  rounded-md focus:outline-none w-full" placeholder="Buscar en reddit...">
+            <div class="w-1/4 h-10 my-auto border border-solid border-gray-300 rounded-sm">
+                <select name="searchOption" class="bg-light-grey  rounded-md h-full w-full">
+                    <option value="community">Comunidad</option>
+                    <option value="post">Post</option>
+                </select>
+            </div>
         </div>
         <div v-if="user">
             <div class="flex border-2 border-gray-700 p-2 px-7 rounded-lg relative" v-if="user">
